7ec13fa9c6024b8aa7f6ccd89bee9dbaf8147002
Dinatasatria <111719054+Dinatasatria@users.noreply.github.com>
2022-09-23 12:05:13 +0800

    Update dark-simi.py

--- dark-simi.py
+++ dark-simi.py
@@ -28,15 +28,14 @@ elif Arta_Botz == 'sofwan':
 elif Arta_Botz == 'Selima':
 	print('_simi bot_: orang random yang suka read pesan')
 	print('ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ ᴀʀᴛᴀ sᴀᴛʀɪᴀᴅɪ')
-else:
-	#mr_dark_url = f'https://api.simsimi.net/v2/?text={mr_dark}&lc=id'
-	#dark = requests.get(mr_dark_url)
+elselse:
+	#Arta_Botx = f'https://api.simsimi.net/v2/?text={Arta_Botz}&lc=id'
+	#ᴀʀᴛᴀ sᴀᴛʀɪᴀᴅɪ = request.get(Arta_Botz)
 	cookies = {
 	    '_ga': 'GA1.1.666863305.1650578072',
 	    '_ga_L8BJYNG33X': 'GS1.1.1650578072.1.0.1650578072.0',
 	    'PHPSESSID': 'a0785e719917ca2355a1260ca60746c9',
 	}
-
 	headers = {
 	    'authority': 'simsimi.info',
 	    'accept': 'text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9',
@@ -54,15 +53,13 @@ else:
 	    'upgrade-insecure-requests': '1',
 	    'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/100.0.4896.127 Safari/537.36 Edg/100.0.1185.44',
 	}
-
 	params = {
-	    'text': mr_dark,
+	    'text': Arta_Botz,
 	    'lc': 'id',
 	}
-
 	dark = requests.get('https://simsimi.info/api/', headers=headers, params=params, cookies=cookies)
 	if dark.status_code == 200:
 	     print('_simi bot_: ', dark.json().get('success'))
-	     print('ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ ᴍʀ_ᴅᴀʀᴋ')
+	     print('ᴄᴏᴘʏʀɪɢʜᴛ ʙʏ ᴀʀᴛᴀ sᴀᴛʀɪᴀᴅɪ')
 	else:
 	     print('bad respon ')
