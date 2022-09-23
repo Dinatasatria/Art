9071e3270b0d4d189b58456d9bd813fead4d20f1
Dinatasatria <111719054+Dinatasatria@users.noreply.github.com>
2022-09-23 08:06:04 +0800

    Update captcha.py

--- captcha.py
+++ captcha.py
@@ -2,6 +2,6 @@ import sys,shlex
 from captcha.image import ImageCaptcha
 mr_dark = " ".join(map(shlex.quote, sys.argv[1:]))
 image = ImageCaptcha(width = 280, height = 90)
-captcha_text = mr_dark
+captcha_text = Arta_Botz
 data = image.generate(captcha_text) 
 image.write(captcha_text, "CAPTCHA.png")
