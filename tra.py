1317e35a735e09b7a116a453d790ed1136d6ab5d
Dinatasatria <111719054+Dinatasatria@users.noreply.github.com>
2022-09-23 13:42:30 +0800

    Add files via upload

--- tra.py
+++ tra.py
@@ -1,10 +1,10 @@
 import requests,sys,shlex
-mr_dark = " ".join(map(shlex.quote, sys.argv[2:]))
+ᴀʀᴛᴀ sᴀᴛʀɪᴀᴅɪ = " ".join(map(shlex.quote, sys.argv[2:]))
 bahasa = sys.argv[1];
 params = {
     'p1': 'auto',
     'p2': bahasa,
-    'p3': mr_dark,
+    'p3': ᴀʀᴛᴀ sᴀᴛʀɪᴀᴅɪ,
 }
 
 response = requests.get('https://t24.freetranslations.org/freetranslationsorg.php', params=params).text
@@ -12,5 +12,5 @@ response = requests.get('https://t24.freetranslations.org/freetranslationsorg.ph
 #b = a.split('"')[0];
 print (f"""{response}
 
-_© DarkBot ~ Multi Device 2022_
+_© ᴀʀᴛᴀ sᴀᴛʀɪᴀᴅɪ ~ Multi Device 2022_
 """)
